780dc1bc6a72cca255854566b5885f0dfc6f5e7b
Nathaniel Collier <nathaniel.collier@aecc.com>
2017-08-24 07:54:44 -0500

    Put in some initial code that doesn't work right for react using typescript

---
+++ systemjs.config.js
@@ -0,0 +1,33 @@
+/**
+ * PLUNKER VERSION
+ * (based on systemjs.config.js in angular.io)
+ * System configuration for Angular samples
+ * Adjust as necessary for your application needs.
+ */
+(function (global) {
+  System.config({
+    // DEMO ONLY! REAL CODE SHOULD NOT TRANSPILE IN THE BROWSER
+    transpiler: 'typescript',
+    typescriptOptions: {
+      tsconfig: true
+    },
+    paths: {
+      // paths serve as alias
+      'npm:': 'https://unpkg.com/'
+    },
+    // map tells the System loader where to look for things
+    map: {
+      // our app is within the app folder
+      app: 'app',
+
+      'typescript':     'npm:typescript@2.2.1/lib/typescript.js'
+    },
+    // packages tells the System loader how to load when no filename and/or no extension
+    packages: {
+      app: {
+        main: './app/app.tsx',
+        defaultExtension: 'tsx'
+      }
+    }
+  });
+})(this);
