d9116d429d0fea4a19183f131c5cf2ba5d6ef6e7
Nathaniel Collier <nathaniel.collier@aecc.com>
2017-08-25 07:18:30 -0500

    Moving the ButtonCounter component into it's own file

--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-class ReactApp extends React.Component {
-  
-  constructor(props) {
-    super(props)
-    this.state = { start: 0, counter: 0 }
-  }
-  
-  render() {
-    return (
-      <div style={{ border: 'solid green 2px', margin: '2px' }}>
-        <h2>Here is in React</h2>
-        <div>
-          <span>Counter: { this.state.start + this.state.counter }</span>
-          <button onClick={ e => this.handleClick(e) }>Click Me!</button>
-        </div>
-      </div>
-    );
-  }
-  
-  
-  handleClick() {
-    const newCounter = this.state.counter + 1
-    this.setState({ counter: newCounter })
-  }
-  
-
-}
+import { ButtonCounter } from './components/ButtonCounter.tsx';
 
 
 ReactDOM.render(
-  <ReactApp ref={ component => this.reactComponent = component }/>,
+  <ButtonCounter ref={ component => this.reactComponent = component }/>,
   document.querySelector('#app') as HTMLElement
 );
 
